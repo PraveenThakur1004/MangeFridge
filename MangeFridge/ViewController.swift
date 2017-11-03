2dc0cec2e10f648c5768276a4e22e33c9e81bbec
Master <mac@Praveen.local>
2017-11-03 17:57:48 +0530

    Initial Commit

---
+++ MangeFridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MangeFridge
+//
+//  Created by MAC on 03/11/17.
+//  Copyright © 2017 Orem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
